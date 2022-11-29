b3dde513aff2c77fc0778c06e86215a54cc09d1e
Peter Juelsgaard <pj@magenta.dk>
2022-11-29 10:47:04 +0100

    Expanding test coverage

--- tests/test_send_email.py
+++ tests/test_send_email.py
@@ -22,32 +22,41 @@ class EmailTest(unittest.TestCase):
         Test that sent email reflects input accurately
         """
 
-        message_args = {
-            "receiver": {"jens@jens.jens", "mads@mads.mads"},
-            "sender": "sine@sine.sine",
-            "cc": ["anders@and.rap", "fedt@mule.hydr"],
-            "bcc": ["rip@rap.rup", "snip@snap.snude"],
-            "subject": "En meget vigtig besked",
-            "body": "Brødteksten på den meget vigtige besked",
-            "run_test": True,
-        }
+        receiver = {"jens@jens.jens", "mads@mads.mads"}
+        sender = "sine@sine.sine"
+        cc = {"anders@and.rap", "fedt@mule.hydr"}
+        bcc = {"rip@rap.rup", "snip@snap.snude"}
+        subject = "En meget vigtig besked"
+        body = "Brødteksten på den meget vigtige besked"
+        texttype = "plain"
 
         # Retrieve MIMEText object from send_email
         message = await send_email.send_email(
-            receiver={"jens@jens.jens", "mads@mads.mads"},
-            sender="sine@sine.sine",
-            cc={"anders@and.rap", "fedt@mule.hydr"},
-            bcc={"rip@rap.rup", "snip@snap.snude"},
-            subject="En meget vigtig besked",
-            body="Brødteksten på den meget vigtige besked",
+            receiver=receiver,
+            sender=sender,
+            cc=cc,
+            bcc=bcc,
+            subject=subject,
+            body=body,
+            texttype=texttype,
         )
+
+        expected_message = MIMEText(body, texttype)
+        expected_message["Subject"] = subject
+        expected_message["From"] = sender
+        expected_message["To"] = ", ".join(receiver)
+        expected_message["CC"] = ", ".join(cc)
+        expected_message["BCC"] = ", ".join(bcc)
 
         # Assert correct return type
         self.assertIsInstance(message, MIMEText)
 
         # Assert correct field mapping
-        self.assertEqual(message["To"], message_args["receiver"])
+        self.assertEqual(message, expected_message)
+        """
+        self.assertEqual(message["To"], receiver)
         self.assertEqual(message["From"], message_args["sender"])
         self.assertEqual(message["CC"], message_args["cc"])
         self.assertEqual(message["BCC"], message_args["bcc"])
         self.assertEqual(message["Subject"], message_args["subject"])
+        """
