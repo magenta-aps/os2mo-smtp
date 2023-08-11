49933af95c20cd6221394e564a3c6d56c8c044ff
Nick Janssen <nj@magenta-aps.dk>
2023-08-11 10:46:03 +0200

    fix: [#56815] extract latest object rather than first

--- mo_smtp/dataloaders.py
+++ mo_smtp/dataloaders.py
@@ -1,13 +1,66 @@
+import datetime
 from typing import Any
+from typing import Union
 from uuid import UUID
 
 from fastramqpi.context import Context
 from gql import gql
 
 
+def mo_datestring_to_utc(datestring: Union[str, None]):
+    """
+    Returns datetime object at UTC+0
+
+    Notes
+    ------
+    Mo datestrings are formatted like this: "2023-02-27T00:00:00+01:00"
+    This function essentially removes the "+01:00" part, which gives a UTC+0 timestamp.
+    """
+    if datestring:
+        return datetime.datetime.fromisoformat(datestring).replace(tzinfo=None)
+    else:
+        return None
+
+
 class DataLoader:
     def __init__(self, context: Context):
         self.gql_client = context["user_context"]["gql_client"]
+
+    @staticmethod
+    def extract_current_or_latest_object(objects: list[dict]):
+        """
+        Check the validity in a list of object dictionaries and return the one which
+        is either valid today, or has the latest end-date
+        """
+
+        if len(objects) == 1:
+            return objects[0]
+        elif len(objects) == 0:
+            raise Exception("Objects is empty")
+        else:
+            # If any of the objects is valid today, return it
+            for obj in objects:
+                valid_to = obj["validity"]["to"]
+                valid_from = obj["validity"]["from"]
+
+                if valid_to and valid_from:
+                    valid_to_utc = mo_datestring_to_utc(valid_to)
+                    valid_from_utc = mo_datestring_to_utc(valid_from)
+                    now_utc = datetime.datetime.utcnow()
+
+                    if now_utc > valid_from_utc and now_utc < valid_to_utc:
+                        return obj
+
+            # If any of the to-dates is None (i.e. valid forever); Return it
+            for obj in objects:
+                if obj["validity"]["to"] is None:
+                    return obj
+
+            # Otherwise return the latest
+            to_dates = [mo_datestring_to_utc(obj["validity"]["to"]) for obj in objects]
+            sorted_objects = [x for _, x in sorted(zip(to_dates, objects))]
+
+            return sorted_objects[-1]
 
     async def load_mo_manager_data(self, uuid: UUID) -> Any:
         query = gql(
@@ -15,14 +68,15 @@ class DataLoader:
             query getManagerData {
               managers(
                   uuids: "%s",
-                  from_date: "1900-01-01"
-                  to_date: "5000-01-01"
+                  from_date: null
+                  to_date: null
               ) {
                 objects {
                   employee_uuid
                   org_unit_uuid
                   validity {
                     to
+                    from
                   }
                 }
               }
@@ -32,7 +86,7 @@ class DataLoader:
         )
 
         result = await self.gql_client.execute(query)
-        return result["managers"][0]["objects"][0]
+        return self.extract_current_or_latest_object(result["managers"][0]["objects"])
 
     async def load_mo_user_data(self, uuid: UUID) -> Any:
         """
