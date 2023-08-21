0010b4cf6e1d7cd401386a63c4be18ecc6cfce34
Nick Janssen <nj@magenta-aps.dk>
2023-08-21 13:43:40 +0200

    fix: [#57197] do not override receiver for the test mail

--- mo_smtp/mail.py
+++ mo_smtp/mail.py
@@ -25,6 +25,7 @@ class EmailClient:
         texttype: str = "plain",
         cc: set[str] = {""},
         bcc: set[str] = {""},
+        allow_receiver_override=True,
     ) -> MIMEText:
         """
         Sends outgoing email given parameters
@@ -55,7 +56,7 @@ class EmailClient:
         msg = MIMEText(body, texttype, _charset="utf-8")
         msg["Subject"] = subject
         msg["From"] = self.sender
-        if self.receiver_override:
+        if self.receiver_override and allow_receiver_override:
             msg["To"] = self.receiver_override
         else:
             msg["To"] = ", ".join(receiver)
